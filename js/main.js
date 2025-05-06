ad1933caffe16bcfa98173240552897937c14fd8
4nthonyZ <achigga2334@gmail.com>
2025-05-07 03:51:59 +1000

    product dertail

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
 // main.js: placeholder for future interactivity
 console.log("Joe Calvi Furniture site loaded.");
+function changeImage(thumbnail) {
+    const mainImage = document.getElementById("mainImage");
+    mainImage.src = thumbnail.src;
+  }
+  
+  
