180149d954b772eadea382b2ceddce06333b7e8b
shuyzhang2 <shuyuan.zhang5@student.unimelb.edu.au>
2025-05-09 17:26:20 +1000

    debug and update

--- js/main.js
+++ js/main.js
@@ -4,5 +4,30 @@ function changeImage(thumbnail) {
     const mainImage = document.getElementById("mainImage");
     mainImage.src = thumbnail.src;
   }
+
+let cartItemCount = parseInt(localStorage.getItem("cartCount")) || 0;
+document.getElementById("cart-count").textContent = cartItemCount;
+
   
+function showCartPopup() {
+  const popup = document.createElement("div");
+  popup.textContent = "✅ Item added to cart!";
+  popup.className = "cart-popup";
+  document.body.appendChild(popup);
+
+  setTimeout(() => {
+    popup.remove();
+  }, 2000); // 自动消失
+  cartItemCount += 1;
+  localStorage.setItem("cartCount", cartItemCount);
+  document.getElementById("cart-count").textContent = cartItemCount;
+}
   
+window.addEventListener("DOMContentLoaded", () => {
+  const count = localStorage.getItem("cartCount") || 0;
+  const cartCountElement = document.getElementById("cart-count");
+  if (cartCountElement) {
+    cartCountElement.textContent = count;
+  }
+});
+
